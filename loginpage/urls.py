b457df68fd50eb7644e4066751c32ab6d24d4541
Mayunk <mdkul22@gmail.com>
2016-05-24 11:14:10 +0530

    new commits

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -1,4 +1,4 @@
-from dajngo.conf.urls import url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
--- loginpage/views.py
+++ loginpage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_button(request):
+    return render(request, 'loginpage/login_button.html', {})
+
