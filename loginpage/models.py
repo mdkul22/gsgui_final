95b1c48f2dcb6ea9c7a80a429f683c3d31f82be6
Mayunk <mdkul22@gmail.com>
2016-05-24 23:33:39 +0530

    An experiment to try

--- loginpage/models.py
+++ loginpage/models.py
@@ -5,14 +5,12 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     username = models.CharField(max_length=200)
-    password = models.TextField()
+    password = models.CharField(max_length=200)
     created_date = models.DateTimeField(
             default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
 
-    def post(self):
-        self.published_date = timezone.now()
+    def creation(self):
+        self.created_date = timezone.now()
         self.save()
 
     def __str__(self):
